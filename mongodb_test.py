6a2e0be215d18a9c1f6f2369040f2388341946a7
RLoos123 <79148088+RLoos123@users.noreply.github.com>
2023-07-02 14:53:38 -0500

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-url = "mongodb+srv://rloos:Cybr410@project42.q8nksg5.mongodb.net/?retryWrites=true&w=majority"
-client = "MongoClient(url)"
-db = "client.pytech"
-print("-- Pytech C0llection List--\n['students']")
