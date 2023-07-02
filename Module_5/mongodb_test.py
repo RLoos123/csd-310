0a5150d1e419654c8abb35251aa17ad94d10e0b9
RLoos123 <79148088+RLoos123@users.noreply.github.com>
2023-07-02 14:49:48 -0500

    Update mongodb_test.py

--- Module_5/mongodb_test.py
+++ Module_5/mongodb_test.py
@@ -1,5 +1,5 @@
 from pymongo import mongo_client
-url = "mongodb+srv://Roymega:Megacubs86!!@cluster0.lqezwyp.mongodb.net/?retryWrites=true&w=majority"
+url = "mongodb+srv://rloos:Cybr410@project42.q8nksg5.mongodb.net/?retryWrites=true&w=majority"
 client = "MongoClient(url)"
 db = "client.pytech"
 print("db.list_collection_names")
